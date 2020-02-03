19b458f004d46fe6bf205815da671576d1f0485a
Nathan <nathan_duran@hotmail.com>
2020-02-03 17:02:36 +0000

    Finish Data Sorting
    Start T-test

--- data_processing/data_utilities.py
+++ data_processing/data_utilities.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from scipy.stats import ttest_ind
 
 
 def save_dataframe(path, data):
@@ -11,6 +12,24 @@ def load_experiment_data(task_name, experiment_type):
     data = pd.read_csv(data_path, index_col=False, header=0, quotechar="'")
     data = data.drop('experiment_name', axis='columns')
     data.model_name = data.model_name.str.replace("_", " ")
+    return data
+
+
+def sort_experiment_data_by_model_and_metric(data, metric):
+    # Model sort order
+    sort_order = ['cnn', 'text cnn', 'dcnn', 'rcnn', 'lstm', 'bi lstm', 'gru', 'bi gru']
+
+    # Create the dictionary that defines the order for sorting
+    sorter_index = dict(zip(sort_order, range(len(sort_order))))
+
+    # Generate a rank column that will be used to sort the dataframe numerically
+    data['model_name_rank'] = data['model_name'].map(sorter_index)
+
+    # Sort by model name (rank) and metric
+    data.sort_values(['model_name_rank', metric],  inplace=True)
+    # Drop rank column
+    data.drop('model_name_rank', 1, inplace=True)
+
     return data
 
 
@@ -35,3 +54,31 @@ def get_experiment_means(data, experiment_type):
             mean_data = mean_data.append(model_data, ignore_index=True)
 
     return mean_data
+
+
+def t_test(data, experiment_type):
+
+    # Get the list of models and ranges of experiment
+    model_names = data['model_name'].unique()
+    experiment_values = data[experiment_type].unique()
+    results_dict = dict()
+    for model in model_names:
+        model_dict = dict()
+        for i in range(len(experiment_values) - 1):
+
+            # Select the data to compare
+            data_a = data.loc[(data['model_name'] == model) & (data[experiment_type] == experiment_values[i])]
+            data_b = data.loc[(data['model_name'] == model) & (data[experiment_type] == experiment_values[i+1])]
+
+            # T-test
+            t_and_p = ttest_ind(data_a['test_acc'], data_b['test_acc'])
+            # Add the p value to this pair in the dict
+            model_dict[str(experiment_values[i]) + " & " + str(experiment_values[i+1])] = t_and_p[1]
+
+        # Add this models results to the dict
+        results_dict[model] = model_dict
+
+    # Create dataframe
+    t_test_frame = pd.DataFrame.from_dict(results_dict, orient='index')
+
+    return t_test_frame
--- data_processing/process_data.py
+++ data_processing/process_data.py
@@ -5,30 +5,43 @@ pd.options.display.width = 0
 task_name = 'swda'
 experiment_type = 'vocab_size'
 
-# TODO Save vocab size .csv ordered by vocab size and model (because I did them out of order)
-#  (also put rcnn after dcnn - here, when running experiments and also in models)
+# Load experiment data
 vocab_size_data = load_experiment_data(task_name, experiment_type)
-print(vocab_size_data['test_acc'].max())
+# Sort by model name and experiment type
+vocab_size_data = sort_experiment_data_by_model_and_metric(vocab_size_data, experiment_type)
+# Save dataframe
+save_dataframe(experiment_type + '_current.csv', vocab_size_data)
+# Generate and save charts # TODO Add saving and plot test and val on same plot?
+# g, fig = plot_implot_chart(vocab_size_data, x="vocab_size", y="val_acc", hue="model_name",
+#                            order=4, num_legend_col=4, y_label='Test Accuracy', x_label='Vocabulary Size',
+#                            legend_loc='lower right', colour='Paired')
+# fig.show()
+# g, fig = plot_implot_chart(vocab_size_data, x="vocab_size", y="test_acc", hue="model_name",
+#                            order=4, num_legend_col=4, y_label='Test Accuracy', x_label='Vocabulary Size',
+#                            legend_loc='lower right', colour='Paired')
+# fig.show()
+
+# Get means over all experiments
 vocab_size_means = get_experiment_means(vocab_size_data, experiment_type)
-
-print(vocab_size_means)
+# print(vocab_size_means)
 print(vocab_size_means['test_acc'].max())
+print(vocab_size_means['val_acc'].max())
 
-# Get best vocab size (val/test) per model into table TODO (add min and max for each)
+# Get best (val acc and test acc/F1) for experiment_type per model into table
 best_val = vocab_size_means.loc[vocab_size_means.groupby(['model_name'], sort=False)['val_acc'].idxmax()]
 best_val.drop(best_val.columns.difference(['model_name', 'vocab_size', 'val_acc']), 1, inplace=True)
 best_val.rename(columns={'vocab_size': 'val_vocab_size'}, inplace=True)
 best_val = best_val.set_index('model_name')
 
 best_test = vocab_size_means.loc[vocab_size_means.groupby(['model_name'], sort=False)['test_acc'].idxmax()]
-best_test.drop(best_test.columns.difference(['model_name', 'vocab_size', 'test_acc']), 1, inplace=True)
+best_test.drop(best_test.columns.difference(['model_name', 'vocab_size', 'test_acc', 'f1_micro', 'f1_weighted']), 1, inplace=True)
 best_test.rename(columns={'vocab_size': 'test_vocab_size'}, inplace=True)
 best_test = best_test.set_index('model_name')
 
 vocab_size_best = pd.concat([best_val, best_test], axis=1, ignore_index=False, sort=False).reset_index()
 print(vocab_size_best)
 
-g, fig = plot_implot_chart(vocab_size_data, x="vocab_size", y="val_acc", hue="model_name",
-                           order=4, num_legend_col=4, y_label='Test Accuracy', x_label='Vocabulary Size',
-                           legend_loc='lower right', colour='Paired')
-fig.show()
+# Test if result pairs are statistically significant # TODO concat val and test data? + graph to show what is significant
+t_test_frame = t_test(vocab_size_data, experiment_type)
+print(t_test_frame)
+# TODO Change order so rcnn is after dcnn (also for models/model_params)
