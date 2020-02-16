b04e496a679359c2913644d69a6e4e31361a4aaa
Nathan <nathan_duran@hotmail.com>
2020-02-16 01:04:12 +0000

    Finish ANOVA Functions

--- data_processing/process_data.py
+++ data_processing/process_data.py
@@ -22,38 +22,49 @@ data = data.drop('experiment_name', axis='columns')
 data.model_name = data.model_name.str.replace("_", " ")
 
 # Sort by model name and experiment type # TODO do i really need to sort future experiments?
-# sort_order = ['cnn', 'text cnn', 'dcnn', 'rcnn', 'lstm', 'bi lstm', 'gru', 'bi gru']
-# data = sort_dataframe_by_list_and_param(data, 'model_name', sort_order, experiment_type)
+sort_order = ['cnn', 'text cnn', 'dcnn', 'rcnn', 'lstm', 'bi lstm', 'gru', 'bi gru']
+data = sort_dataframe_by_list_and_param(data, 'model_name', sort_order, experiment_type)
 # Save dataframe with all the data in
-# save_dataframe(os.path.join(output_dir, experiment_type + '_data_raw.csv'), data)
+save_dataframe(os.path.join(output_dir, experiment_type + '_data_raw.csv'), data)
 
 # Get means over all experiments
-# data_means = get_means(data, experiment_type)
-# save_dataframe(os.path.join(output_dir, experiment_type + '_data_means.csv'), data_means)
+data_means = get_means(data, experiment_type)
+save_dataframe(os.path.join(output_dir, experiment_type + '_data_means.csv'), data_means)
 
-# # Get test and validation accuracy for each model
-# acc_data = data.drop(data.columns.difference(['model_name', experiment_type, 'val_acc', 'test_acc']), axis=1)
-# acc_data = acc_data.rename(columns={'val_acc': 'Val Acc', 'test_acc': 'Test Acc'})
-# acc_data = acc_data.melt(id_vars=['model_name', 'vocab_size'])
-#
-# g, fig = plot_lmplot_chart(acc_data, x="vocab_size", y="value", hue="model_name", col='variable',
-#                            order=5, num_legend_col=4, y_label='Accuracy', x_label='Vocabulary Size',
-#                            share_x=True, num_col=1, colour='Paired')
-# fig.show()
-# g.savefig(os.path.join(output_dir, experiment_type + '_accuracy.png'))
+# Get test and validation accuracy for each model
+acc_data = data.drop(data.columns.difference(['model_name', experiment_type, 'val_acc', 'test_acc']), axis=1)
+acc_data = acc_data.rename(columns={'val_acc': 'Val Acc', 'test_acc': 'Test Acc'})
+acc_data = acc_data.melt(id_vars=['model_name', 'vocab_size'])
+
+g, fig = plot_lmplot_chart(acc_data, x="vocab_size", y="value", hue="model_name", col='variable',
+                           order=5, num_legend_col=4, y_label='Accuracy', x_label='Vocabulary Size',
+                           share_x=True, num_col=1, colour='Paired')
+fig.show()
+g.savefig(os.path.join(output_dir, experiment_type + '_accuracy.png'))
 
 # # Get max val_acc and test_acc/F1 for experiment_type per model into table
-# max_of_raw_data = get_max(data, experiment_type) # TODO Don't bother with raw data?
-# print(max_of_raw_data)
-# fig = plot_table(max_of_raw_data)
-# fig.show()
-#
-# max_of_mean_data = get_max(data_means, experiment_type)
-# print(max_of_mean_data)
-# fig = plot_table(max_of_mean_data)
-# fig.show()
+print("========================= Raw Data =========================")
+max_of_raw_data = get_max(data, experiment_type) # TODO Don't bother with raw data?
+print(max_of_raw_data)
+print("Best validation accuracy in raw data:")
+print(max_of_raw_data.loc[[max_of_raw_data['val acc'].idxmax()], ['model name', 'val vocab size', 'val acc']])
+print("Best test accuracy in raw data:")
+print(max_of_raw_data.loc[[max_of_raw_data['test acc'].idxmax()], ['model name', 'test vocab size', 'test acc', 'f1 micro', 'f1 weighted']])
+save_dataframe(os.path.join(output_dir, experiment_type + 'max_of_raw_data.csv'), max_of_raw_data)
+fig = plot_table(max_of_raw_data)
+fig.show()
 
-# TODO concat val and test data? + graph to show what is significant
+print("========================= Mean Data =========================")
+max_of_mean_data = get_max(data_means, experiment_type)
+print(max_of_mean_data)
+print("Best validation accuracy in mean data:")
+print(max_of_mean_data.loc[[max_of_mean_data['val acc'].idxmax()], ['model name', 'val vocab size', 'val acc']])
+print("Best test accuracy in mean data:")
+print(max_of_mean_data.loc[[max_of_mean_data['test acc'].idxmax()], ['model name', 'test vocab size', 'test acc', 'f1 micro', 'f1 weighted']])
+save_dataframe(os.path.join(output_dir, experiment_type + '_max_of_mean_data.csv'), max_of_mean_data)
+fig = plot_table(max_of_mean_data)
+fig.show()
+
 # Pairwise t-test between experiment parameters
 # Bonferroni correction post-hoc comparison
 # p-value/# of comparisons = 0.05/15 = 0.00333
@@ -69,5 +80,16 @@ data.model_name = data.model_name.str.replace("_", " ")
 # print(anova_test_frame)
 
 # Tukeys HSD post-hoc comparison
-tukey_frame = tukey_hsd(data, experiment_type, 'val_acc')
-print(tukey_frame)
+for metric in ['val_acc', 'test_acc']:
+    # Calculate the anova
+    tukey_frame = tukey_hsd(data, experiment_type, metric)
+    save_dataframe(os.path.join(output_dir, experiment_type + '_' + metric + '_anova.csv'), tukey_frame)
+
+    # Drop the un-needed columns and generate heatmaps
+    tukey_frame = tukey_frame.drop(columns=['meandiff', 'lower', 'upper', 'reject'], axis=1)
+    g, fig = plot_facetgrid(tukey_frame, x='group1', y='group2', hue='p-value', col='model_name', kind='heatmap',
+                            title='', y_label='', x_label='', num_col=2, colour='RdBu_r',
+                            annot=True, fmt='0.3', linewidths=0.5, cbar=False, custom_boundaries=[0.0, 0.05, 1.0],
+                            y_tick_rotation=45)
+    fig.show()
+    g.savefig(os.path.join(output_dir, experiment_type + '_' + metric + '_anova.png'))
